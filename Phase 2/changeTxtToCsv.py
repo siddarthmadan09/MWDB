2b7df3a07e561dbf66962f6a1b6dfe14d5394eae
vivekpanigrahy <vivek.panigrahy76@gmail.com>
2018-10-28 22:04:06 -0700

    Converting .txt to .csv

--- Phase 2/changeTxtToCsv.py
+++ Phase 2/changeTxtToCsv.py
@@ -1,6 +1,17 @@
 import csv
 
-with open('devset_textTermsPerUser.txt') as fin, open('devset_textTermsPerUserNew.csv', 'w') as fout:
+with open('./data/devset_textTermsPerUser.txt') as fin, open('./data/devset_textTermsPerUser.csv', 'w') as fout:
     o=csv.writer(fout)
     for line in fin:
         o.writerow(line.split())
+
+with open('./data/devset_textTermsPerImage.txt') as fin, open('./data/devset_textTermsPerImage.csv', 'w') as fout:
+    o=csv.writer(fout)
+    for line in fin:
+        o.writerow(line.split())
+
+with open('./data/devset_textTermsPerPOI.wFolderNames.txt') as fin, open('./data/devset_textTermsPerPOI.wFolderNames.csv', 'w') as fout:
+    o=csv.writer(fout)
+    for line in fin:
+        o.writerow(line.split())
+
