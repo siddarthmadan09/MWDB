e87488bd5670ca3c5eccde2241d297161026570e
Sumeet Bhalla <sumeetbhalla@sumeets-air.mobile.asu.edu>
2018-11-21 20:24:59 -0700

    updated main file new

--- Phase 3/mainFile.py
+++ Phase 3/mainFile.py
@@ -23,87 +23,87 @@ from sklearn.metrics.pairwise import cosine_similarity as cosineSimilarity
 from sklearn.metrics.pairwise import euclidean_distances as euclideanDistance
 from sklearn.metrics.pairwise import manhattan_distances as manhattanDistance
 
-class Graph:
-
-    def __init__(self, graph):
-        self.graph = graph  # residual graph
-        self.org_graph = [i[:] for i in graph]
-        self.ROW = len(graph)
-        self.COL = len(graph[0])
-
-    '''Returns true if there is a path from source 's' to sink 't' in 
-    residual graph. Also fills parent[] to store the path '''
-
-    def BFS(self, s, t, parent):
-
-        # Mark all the vertices as not visited
-        visited = [False] * (self.ROW)
-
-        # Create a queue for BFS
-        queue = []
-
-        # Mark the source node as visited and enqueue it
-        queue.append(s)
-        visited[s] = True
-
-        # Standard BFS Loop
-        while queue:
-
-            # Dequeue a vertex from queue and print it
-            u = queue.pop(0)
-
-            # Get all adjacent vertices of the dequeued vertex u
-            # If a adjacent has not been visited, then mark it
-            # visited and enqueue it
-            for ind, val in enumerate(self.graph[u]):
-                if visited[ind] == False and val > 0:
-                    queue.append(ind)
-                    visited[ind] = True
-                    parent[ind] = u
-
-                    # If we reached sink in BFS starting from source, then return
-        # true, else false
-        return True if visited[t] else False
-
-    # Returns the min-cut of the given graph
-    def minCut(self, source, sink):
-
-        # This array is filled by BFS and to store path
-        parent = [-1] * (self.ROW)
-
-        max_flow = 0  # There is no flow initially
-
-        # Augment the flow while there is path from source to sink
-        while self.BFS(source, sink, parent):
-
-            # Find minimum residual capacity of the edges along the
-            # path filled by BFS. Or we can say find the maximum flow
-            # through the path found.
-            path_flow = float("Inf")
-            s = sink
-            while (s != source):
-                path_flow = min(path_flow, self.graph[parent[s]][s])
-                s = parent[s]
-
-                # Add path flow to overall flow
-            max_flow += path_flow
-
-            # update residual capacities of the edges and reverse edges
-            # along the path
-            v = sink
-            while (v != source):
-                u = parent[v]
-                self.graph[u][v] -= path_flow
-                self.graph[v][u] += path_flow
-                v = parent[v]
-
-                # print the edges which initially had weights
-        # but now have 0 weight
-        for i in range(self.ROW):
-            for j in range(self.COL):
-                if self.graph[i][j] == 0 and self.org_graph[i][j] > 0:
-                    print
-                    str(i) + " - " + str(j)
+# class Graph:
+#
+#     def __init__(self, graph):
+#         self.graph = graph  # residual graph
+#         self.org_graph = [i[:] for i in graph]
+#         self.ROW = len(graph)
+#         self.COL = len(graph[0])
+#
+#     '''Returns true if there is a path from source 's' to sink 't' in
+#     residual graph. Also fills parent[] to store the path '''
+#
+#     def BFS(self, s, t, parent):
+#
+#         # Mark all the vertices as not visited
+#         visited = [False] * (self.ROW)
+#
+#         # Create a queue for BFS
+#         queue = []
+#
+#         # Mark the source node as visited and enqueue it
+#         queue.append(s)
+#         visited[s] = True
+#
+#         # Standard BFS Loop
+#         while queue:
+#
+#             # Dequeue a vertex from queue and print it
+#             u = queue.pop(0)
+#
+#             # Get all adjacent vertices of the dequeued vertex u
+#             # If a adjacent has not been visited, then mark it
+#             # visited and enqueue it
+#             for ind, val in enumerate(self.graph[u]):
+#                 if visited[ind] == False and val > 0:
+#                     queue.append(ind)
+#                     visited[ind] = True
+#                     parent[ind] = u
+#
+#                     # If we reached sink in BFS starting from source, then return
+#         # true, else false
+#         return True if visited[t] else False
+#
+#     # Returns the min-cut of the given graph
+#     def minCut(self, source, sink):
+#
+#         # This array is filled by BFS and to store path
+#         parent = [-1] * (self.ROW)
+#
+#         max_flow = 0  # There is no flow initially
+#
+#         # Augment the flow while there is path from source to sink
+#         while self.BFS(source, sink, parent):
+#
+#             # Find minimum residual capacity of the edges along the
+#             # path filled by BFS. Or we can say find the maximum flow
+#             # through the path found.
+#             path_flow = float("Inf")
+#             s = sink
+#             while (s != source):
+#                 path_flow = min(path_flow, self.graph[parent[s]][s])
+#                 s = parent[s]
+#
+#                 # Add path flow to overall flow
+#             max_flow += path_flow
+#
+#             # update residual capacities of the edges and reverse edges
+#             # along the path
+#             v = sink
+#             while (v != source):
+#                 u = parent[v]
+#                 self.graph[u][v] -= path_flow
+#                 self.graph[v][u] += path_flow
+#                 v = parent[v]
+#
+#                 # print the edges which initially had weights
+#         # but now have 0 weight
+#         for i in range(self.ROW):
+#             for j in range(self.COL):
+#                 if self.graph[i][j] == 0 and self.org_graph[i][j] > 0:
+#                     print
+#                     str(i) + " - " + str(j)
 
 # def returnUserTerms(row):
 #     col_count = 1
@@ -391,11 +391,9 @@ for idx, fileCluster in enumerate(otherLocationFileCluster):
         allImageIDs.extend(imageIdArr)
     else:
         finalImageFeatureMatrix = np.append(finalImageFeatureMatrix, allModalArray, axis=0)
-        #allImageIDs = np.append(allImageIDs, imageIdArr, axis=0)
         allImageIDs.extend(imageIdArr)
 
 count=0;
-#allUniqueTerms = getAllUniqueTerms(fileName)
 print("\nAll Image Featur Matrix Created")
 print ("Total Time taken to Execute")
 print (str(datetime.datetime.now()-startTime))
@@ -404,15 +402,12 @@ startTime = datetime.datetime.now()
 similarityfileexists = os.path.isfile("similaritymatrix.npy")
 if not similarityfileexists:
     similarityMatrix = euclideanDistance(finalImageFeatureMatrix)
-    # pickling_on = open("similaritymatrix.pickle", "wb")
-    # pickle.dump(imageTermMatrix, pickling_on )
-    # pickling_on.close()
+    # similarityMatrix = cosineSimilarity(finalImageFeatureMatrix)
+    # similarityMatrix = manhattanDistance(finalImageFeatureMatrix)
     numpy.save("similaritymatrix", similarityMatrix)
 else:
     similarityMatrix = numpy.load("similaritymatrix.npy")
-#similarityMatrix = cosineSimilarity(imageTermMatrix)
-# similarityMatrix = manhattanDistance(imageTermMatrix)
-#gc.collect()
+
 print("\nEuclidian similarty done/loaded")
 print ("Total Time taken to Execute")
 print (str(datetime.datetime.now()-startTime))
@@ -429,7 +424,6 @@ while taskNumber>0:
         if not task1output:
             k = (int)(input("Enter value for K = "))
             #calculating most similar k values for each image
-            # G = nx.Graph()
 
             for idx,row in enumerate(similarityMatrix):
                 outputDict[allImageIDs[idx]] = getKMostSimilarImagesAndScoresAsDict(row,k)
@@ -451,7 +445,6 @@ while taskNumber>0:
 
         print("Task 2 code here")
         c = (int)(input("Enter number of Clusters c = "))
-        #localImageIDs = np.asarray(allImageIDs)
         #creating a sparse matrix using the task 1 reduced graph
         imageImageSparse = numpy.zeros(similarityMatrix.shape)
         for idx, key1 in enumerate(outputDict):
